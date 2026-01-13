6ed858da1f9b071dff3eadc04c27da46dac5a6e8
admrl11 <a.mishurniaev@icloud.com>
2026-01-13 13:32:38 +0000

    24594

---
+++ FredsBoats.Web/Migrations/20260113124500_newDatabase.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FredsBoats.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class newDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "telephone",
+                table: "customer",
+                type: "TEXT",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "address",
+                table: "customer",
+                type: "TEXT",
+                maxLength: 250,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 250,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "telephone",
+                table: "customer",
+                type: "TEXT",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "address",
+                table: "customer",
+                type: "TEXT",
+                maxLength: 250,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 250);
+        }
+    }
+}
--- FredsBoats.Web/Models/Customer.cs
+++ FredsBoats.Web/Models/Customer.cs
@@ -7,7 +7,24 @@ namespace FredsBoats.Web.Models
     public class Customer
     {
         // ACSC384 - To Do - Customer Class
-
+        [Key]
+        [Column("customerid")]
+        public int CustomerId { get; set; }
+        [Column("surname")]
+        [StringLength(70)]
+        public string Surname { get; set; } = string.Empty;
+        [Column("name")]
+        [StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+        [Column("address")]
+        [StringLength(250)]
+        public string Address { get; set; } = string.Empty;
+        [Column("telephone")]
+        [StringLength(20)]
+        public string Telephone { get; set; } = string.Empty;
+        [Column("licence")]
+        public int Licence { get; set; }
+        // Navigation
         // Navigation
         public ICollection<CustReservation> CustReservations { get; set; } = new List<CustReservation>();
     }
